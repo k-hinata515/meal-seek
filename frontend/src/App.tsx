d8eba9a2e796fc51941d43653bb575ef9c4a0f0f
AluFu <0hp3261131m102u@ezweb.ne.jp>
2025-06-04 13:04:39 +0900

    feat: 店舗詳細画面のルートを定義

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import SearchPage from './pages/SearchPage';
 import ResultsPage from './pages/ResultsPage';
+import DetailPage from './pages/ShopDetailPage';
+
 function App() {
   return (
     <Router>
@@ -9,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="/results" element={<ResultsPage />} />
+          <Route path="/shop/:shopId" element={<DetailPage />} />
         </Routes>
       </MainLayout>
     </Router>
