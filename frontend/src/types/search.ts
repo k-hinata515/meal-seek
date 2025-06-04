01d9e1531433e6fd8c8218331207a8f02097a1b0
AluFu <0hp3261131m102u@ezweb.ne.jp>
2025-06-04 13:06:59 +0900

    feat: 取得うる店舗の情報を追加

--- frontend/src/types/search.ts
+++ frontend/src/types/search.ts
@@ -7,13 +7,29 @@ export interface SearchParams {
     lng?: number;
 }
 
+// 店舗の予算情報を保持する型
+export interface ShopBudget {
+    code?: string;
+    name?: string;
+    average?: string;
+}
+
+// 店舗のURL情報を保持する型
+export interface ShopUrls {
+    pc?: string; 
+    sp?: string;
+}
+
 // 店舗情報の型 
 export interface ShopType {
+    catch: string;
     id: string;
     name: string;
+    address: string; 
     genre: {
         name: string;
         code: string;
+        catch?: string;  
     };
     mobile_access: string;
     photo: {
@@ -29,6 +45,21 @@ export interface ShopType {
     }
     lat: number;
     lng: number;
+    open?: string;
+    close?: string; 
+    budget?: ShopBudget;
+    coupon_urls?: { 
+        pc?: string;
+        sp?: string;
+    };
+    urls?: ShopUrls; 
+    card?: string;
+    wifi?: string;
+    non_smoking?: string;
+    private_room?: string;
+    free_food?: string;
+    free_drink?: string; 
+    parking?: string;
 }
 
 // タグの型
